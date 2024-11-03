2572680100c6789500aaf545d30f005d3e140ec8
Dustin Underwood <underw33@uwm.edu>
2024-11-03 09:42:50 -0600

    Javadoc updates & addition of some Exception throws

--- src/edu/uwm/cs351/SortledtonGraph.java
+++ src/edu/uwm/cs351/SortledtonGraph.java
@@ -4,10 +4,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 
-//Sources:
-//	Sortledton C++ implementation by Per Fuchs et al: https://gitlab.db.in.tum.de/per.fuchs/sortledton
-//		This was used as a guide for design decisions for our implementation of modified scope.
-
 /**
  * SortledtonGraph is the main class for managing the Sortledton graph data structure.
  * This graph structure supports efficient neighborhood access for both small and large
@@ -15,10 +11,14 @@ import java.util.HashMap;
  * 
  * This class provides operations for adding and removing vertices and edges, retrieving
  * neighborhoods, and finding intersections between neighborhoods.
+ * 
+ * <p>Sources:</p>
+ * <ul>
+ * 	<li>Sortledton C++ implementation by Per Fuchs et al: 
+ * 	<a href= "https://gitlab.db.in.tum.de/per.fuchs/sortledton"> Sortledton GitLab repository</a></li>
+ *</ul>
  *
  * @param <T> The type of the vertex ID
- * 
- * TODO cite C++ implementation, which I've used to guide my work, starting 
  */
 public class SortledtonGraph<T> {
 	//Constants
@@ -45,42 +45,61 @@ public class SortledtonGraph<T> {
 	/**
 	 * Retrieves the neighbors of the given vertex.
 	 *
-	 * @param id The vertex ID.
-	 * @return A list of IDs representing the neighbors of the specified vertex.
+	 * @param id The vertex ID for which to retrieve neighbors.
+	 * @return 	A list of IDs representing the neighbors of the specified vertex,
+	 * 			or an empty list if the vertex has no neighbors or does not exist.
 	 */
 	public List<T> getNeighbors(T id) { 
 		return null;
 	}
 
 	/**
-	 * Inserts an edge between two vertices. Creates vertices if they don’t already exist.
+	 * Inserts an edge between two vertices. Creates vertices automatically if they don’t already exist.
 	 *
 	 * @param srcId The source vertex ID.
 	 * @param destId The destination vertex ID.
+	 * @throws IllegalArgumentException if either ID is null
 	 */
-	public void insertEdge(T srcId, T destId) { /*...*/ }
+	public void insertEdge(T srcId, T destId) { 
+		if (srcId == null || destId == null) throw new IllegalArgumentException("@insertEdge, the parameters, srcID and destID may not be null.");
+		/*...*/ 
+	}
 
 	/**
 	 * Deletes an edge between two vertices if it exists.
 	 *
 	 * @param srcId The source vertex ID.
 	 * @param destId The destination vertex ID.
+	 * @throws IllegalArgumentException if either ID is null
 	 */
-	public void deleteEdge(T srcId, T destId) { /*...*/ }
+	public void deleteEdge(T srcId, T destId) { 
+		if (srcId == null || destId == null) throw new IllegalArgumentException("@deleteEdge, the parameters, srcID and destID may not be null.");
+		/*...*/ 
+	}
 
 	/**
 	 * Inserts a new vertex in the graph.
 	 *
-	 * @param id The vertex ID.
+	 * @param id The vertex ID to insert.
+	 * @throws IllegalArgumentException if the vertex ID is null.
+	 * @throws IllegalStateException if the vertex already exists.
 	 */
-	public void insertVertex(T id) { /*...*/ }
+	public void insertVertex(T id) { 
+		if (id == null) throw new IllegalArgumentException("@insertVertex, the parameter, id, may not be null.");
+		//TODO add check for an extant vertex and throw ISE
+		/*...*/ 
+	}
 
 	/**
 	 * Deletes a vertex and all its associated edges from the graph.
 	 *
 	 * @param id The vertex ID to remove.
+	 * @throws IllegalArgumentException if the vertex ID is null.
 	 */
-	public void deleteVertex(T id) { /*...*/ }
+	public void deleteVertex(T id) {
+		if (id == null) throw new IllegalArgumentException("@insertVertex, the parameter, id, may not be null.");
+		/*...*/ 
+	}
 
 	/**
 	 * Checks if an edge exists between two vertices.
@@ -88,8 +107,10 @@ public class SortledtonGraph<T> {
 	 * @param srcId The source vertex ID.
 	 * @param destId The destination vertex ID.
 	 * @return True if the edge exists, otherwise false.
+	 * @throws IllegalArgumentException if the srcID is null.
 	 */
-	public boolean findEdge(T srcId, T destId) { 
+	public boolean findEdge(T srcId, T destId) {
+		if (srcId == null) throw new IllegalArgumentException("@findEdge, the parameter, srcId, may not be null.");
 		/*...*/ 
 		return true;
 	}
@@ -115,7 +136,8 @@ public class SortledtonGraph<T> {
         private int adjacencySetSize;
 
         /**
-         * Initializes a new VertexEntry, with adjacency set size = zero.
+         * Initializes a new VertexEntry, with adjacency set size set to zero.
+         * The adjacency set represents the number of neighbors connected to this vertex.
          */
         public VertexEntry() {
             adjacencySetSize = 0;
@@ -124,7 +146,7 @@ public class SortledtonGraph<T> {
         /**
          * Gets the size of the adjacency set for this vertex.
          *
-         * @return the size of the adjacency set
+         * @return the number of neighbors for this vertex.
          */
         public int getAdjacencySetSize() {
             return adjacencySetSize;
