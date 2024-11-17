40542d2525bbac0c7c91f323b0e2260337560f8f
Jesse Tessmer <JTessmer@users.noreply.github.com>
2024-11-17 12:03:24 -0600

    Implement Sortledton TestInternals

--- src/test/sortledtongraph/TestInternals.java
+++ src/test/sortledtongraph/TestInternals.java
@@ -1,10 +1,27 @@
 package test.sortledtongraph;
 
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.uwm.cs351.SortledtonGraph;
+import edu.uwm.cs351.SortledtonGraph.Spy;
+import edu.uwm.cs351.SortledtonGraph.Spy.VertexEntry;
+
 public class TestInternals {
 
     // ========== Tests of SortledtonGraph() ==========
 
-    // TODO
+    @Test
+    public void testSortledtonGraph01() {
+        SortledtonGraph<Integer> instance = Spy.newInstance(0, new HashMap<Integer, Integer>(0,1), new VertexEntry[0]);
+
+        boolean isWellFormed = Spy.wellFormed(instance);
+
+        Assert.assertTrue(isWellFormed);
+    }
+
 
     // ========== Tests of wellFormed() ==========
 
