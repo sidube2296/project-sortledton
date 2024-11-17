361c61768deb63301320f6b630622598fd233a24
Jesse Tessmer <JTessmer@users.noreply.github.com>
2024-11-17 11:17:55 -0600

    Implement Sortledton TestNeighbors

--- src/test/sortledtongraph/TestNeighbors.java
+++ src/test/sortledtongraph/TestNeighbors.java
@@ -1,13 +1,34 @@
 package test.sortledtongraph;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import edu.uwm.cs351.SortledtonGraph;
+
 public class TestNeighbors {
 
     // ========== Tests of getNeighbors() ==========
 
-    // TODO
+    @Test
+    public void testGetNeighbors01() {
+        SortledtonGraph<Integer> sortledtonGraph = new SortledtonGraph<Integer>();
+
+        List<Integer> result = sortledtonGraph.getNeighbors(0);
+
+        Assert.assertEquals(result, null);
+    }
 
 
     // ========== Tests of intersectNeighbors() ==========
 
-    // TODO
+    @Test
+    public void testIntersectNeighbors01() {
+        SortledtonGraph<Integer> sortledtonGraph = new SortledtonGraph<Integer>();
+
+        List<Integer> result = sortledtonGraph.intersectNeighbors(123, 456);
+
+        Assert.assertEquals(result, null);
+    }
 }
