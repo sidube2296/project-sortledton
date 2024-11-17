faa4aabfb3416223976bce25e2a0b1ace76f3a1a
Jesse Tessmer <JTessmer@users.noreply.github.com>
2024-11-17 11:37:03 -0600

    Implement Sortledton TestIds

--- src/test/sortledtongraph/TestIds.java
+++ src/test/sortledtongraph/TestIds.java
@@ -1,12 +1,34 @@
 package test.sortledtongraph;
 
+import org.junit.Test;
+import org.junit.Assert;
+
+import edu.uwm.cs351.SortledtonGraph;
+
+
 public class TestIds {
 
-    // ========== Tests of logicalId ==========
+    // ========== Tests of logicalId() ==========
 
-    // TODO
+    @Test
+    public void testLogicalId() {
+        SortledtonGraph<Integer> sortledtonGraph = new SortledtonGraph<Integer>();
+
+        int result = sortledtonGraph.logicalId(123);
+
+        Assert.assertEquals(result, 0);
+    }
 
     // ========== Tests of physicalId() ==========
 
-    // TODO
+    @Test
+    public void testPhysicalId() {
+        SortledtonGraph<Integer> sortledtonGraph = new SortledtonGraph<Integer>();
+
+        try {
+            sortledtonGraph.physicalId(123);
+        } catch (NullPointerException e) {
+            Assert.assertEquals("Cannot invoke \"java.lang.Integer.intValue()\" because the return value of \"java.util.HashMap.get(Object)\" is null", e.getMessage());
+        }
+    }
 }
